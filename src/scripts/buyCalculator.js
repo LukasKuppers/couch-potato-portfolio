3a969d3f4fdcb79a63ed9514c0c5e71956f85320
Lukas Kuppers <lkuppers11@gmail.com>
2022-03-20 21:11:36 -0400

    Create function to calculate buy orders

--- src/scripts/buyCalculator.js
+++ src/scripts/buyCalculator.js
@@ -8,9 +8,30 @@ const distributeFunds = (portfolioInfo, newCapital) => {
         return {};
     }
 
-    let new_volume = {}
+    let new_volume = {};
+    let max_share_price = 0;
+    let current_portfolio_value = 0;
     for (const ticker in portfolioInfo) {
+        const securityInfo = portfolioInfo[ticker];
         new_volume[ticker] = 0;
+        max_share_price = Math.max(max_share_price, securityInfo.price);
+        current_portfolio_value += securityInfo.volume * securityInfo.price;
+    }
+
+    while (newCapital > max_share_price) {
+        let allocDiff = {}
+        Object.entries(portfolioInfo).forEach(([ticker, values]) => {
+            const currentVolume = values.volume + new_volume[ticker]
+            const currentAlloc = currentVolume * values.price / current_portfolio_value * 100;
+            allocDiff[ticker] = values.desiredAlloc - currentAlloc;
+        });
+
+        let ticker_to_buy = Object.keys(allocDiff).reduce((a, b) => {
+            return allocDiff[a] > allocDiff[b] ? a : b;
+        });
+        new_volume[ticker_to_buy] += 1;
+        current_portfolio_value += portfolioInfo[ticker_to_buy].price;
+        newCapital -= portfolioInfo[ticker_to_buy].price;
     }
     return new_volume;
 };
