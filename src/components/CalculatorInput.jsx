368fd9b8c7384ad0c27534493e3b055d6ff6c577
Lukas Kuppers <lkuppers11@gmail.com>
2022-03-25 20:03:37 -0400

    Display portfolio calculation result

--- src/components/CalculatorInput.jsx
+++ src/components/CalculatorInput.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ShareSelector from "./ShareSelector";
+import distributeFunds from "../scripts/buyCalculator";
 
 const defaultFormData = {
     ticker: '', 
@@ -11,6 +12,9 @@ const defaultFormData = {
 const CalculatorInput = () => {
     const [nextFormID, setNextFormID] = useState(2);
     const [forms, setForms] = useState({1: defaultFormData});
+
+    const [capital, setCapital] = useState(0);
+    const [calcResult, setCalcResult] = useState({});
 
     const addSelector = () => {
         let newForms = structuredClone(forms);
@@ -31,6 +35,21 @@ const CalculatorInput = () => {
         setForms(newForms);
     }
 
+    const calculateResult = () => {
+        // transform form data
+        let calcInput = {};
+        Object.values(forms).forEach(formData => {
+            calcInput[formData.ticker] = {
+                price: Number(formData.price), 
+                volume: parseInt(formData.volume), 
+                desiredAlloc: Number(formData.desiredAlloc)
+            };
+        });
+
+        const calcResult = distributeFunds(calcInput, capital);
+        setCalcResult(calcResult);
+    }
+
     const renderForms = () => {
         return (
             <ul>
@@ -49,8 +68,17 @@ const CalculatorInput = () => {
 
     return (
         <div className="calculator-input">
+            <label>
+                Amount of New Capital:
+                <input 
+                    type='number' 
+                    value={capital} 
+                    onChange={(e) => setCapital(e.target.value)} />
+            </label>
             {renderForms()}
             <button onClick={addSelector}>ADD</button>
+            <button onClick={calculateResult}>CALC</button>
+            {JSON.stringify(calcResult)}
         </div>
     );
 }
