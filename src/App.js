6f0ede917e8a0536f1c87e0ab3a2271abb64e58a
Lukas Kuppers <lkuppers11@gmail.com>
2022-03-25 21:02:51 -0400

    Style navbar

--- src/App.js
+++ src/App.js
@@ -12,14 +12,26 @@ const App = () => {
 
     return (
         <div className="app">
-            <div className = 'app-bar'>
-                <h1 className='text-3xl font-bold'>
-                    PORTFOLIO HELPER
-                </h1>
-                <a href='/home'>HOME</a>
-                <br />
-                <a href='/buy-calculator'>CALCULATOR</a>
-            </div>
+            <nav className='bg-gray-800'>
+                <div className='max-w-7xl  px-2 sm:px-6 lg:px-8'>
+                    <div className='relative flex items-center justify-between h-16'>
+                        <div className='flex-1 flex items-center justify-center sm:items-stretch sm:justify-start'>
+                            <div className='hidden flex-shrink-0 items-center sm:block'>
+                                <span className='text-white text-lg font-extrabold'>DIY PORTFOLIO CALCULATOR</span>
+                            </div>
+                            <div className='flex sm:block sm:ml-6'>
+                                <div className='flex space-x-4'>
+                                    <a href='/home' className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'>
+                                        Home</a>
+                                    <a href='/buy-calculator' className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'>
+                                        Buy Calculator</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
             { page }
         </div>
     );
